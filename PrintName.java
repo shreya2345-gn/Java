e8544fee4b7c8ee1b8981103837f18e096b95ac5
shreya2345-gn <dhanyya3334@gmail.com>
2025-04-08 01:03:30 -0400

    saved

---
+++ PrintName.java
@@ -0,0 +1,16 @@
+package cie277;
+
+import java.util.Scanner;
+
+public class PrintName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
+}
